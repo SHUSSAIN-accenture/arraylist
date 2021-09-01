e949b26fcf613a875b036094cbccfa806594db59
SHUSSAIN-accenture <89249437+SHUSSAIN-accenture@users.noreply.github.com>
2021-09-01 16:09:48 +0100

    Add files via upload
    arraylist excercise

---
+++ ArraylistEx1.java
@@ -0,0 +1,71 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+
+public class ArraylistEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QAlists();
+
+	}
+	
+	public static void QAlists() {
+		ArrayList<String> OrdersArray = new ArrayList<>();
+		OrdersArray.add("0.Water w/ ice");
+		OrdersArray.add("1.Small Espresso w/ Chocolate");
+		OrdersArray.add("2.Large Cold Brew w/ double shot");
+		OrdersArray.add("3.Medium Cappucino w/ Oat Milk");
+		OrdersArray.add("4.Medium Flat White");
+		OrdersArray.add("5.Large Choc Cookie Frappe w/ sugar free syrup");
+		OrdersArray.add("6.Large Cafe Au Lait w/ extra milk");
+
+
+
+		System.out.println(OrdersArray);
+		System.out.println("============================");
+		System.out.println("Order number 5 - " +OrdersArray.get(5));
+		OrdersArray.set(3,"Medium Cappucino w/ Whole Milk");
+		System.out.println("============================");
+		System.out.println("Changed order -" +OrdersArray);
+
+
+
+		ArrayList<String> DoneOrdersArray = new ArrayList<>();
+		DoneOrdersArray.add(OrdersArray.get(1));
+		DoneOrdersArray.add(OrdersArray.get(2));
+		System.out.println("============================");
+		System.out.println("Orders Done - " +DoneOrdersArray);
+		OrdersArray.remove(1);
+		OrdersArray.remove(2);
+		System.out.println("============================");
+		System.out.println("Orders Left - " +OrdersArray);
+		System.out.println("============================");
+		System.out.println("No. of orders left -" +OrdersArray.size());
+		System.out.println("No. of orders done -" +DoneOrdersArray.size());
+
+
+
+		DoneOrdersArray.add(OrdersArray.get(0));
+		DoneOrdersArray.add(OrdersArray.get(1));
+		DoneOrdersArray.add(OrdersArray.get(2));
+		DoneOrdersArray.add(OrdersArray.get(3));
+		DoneOrdersArray.add(OrdersArray.get(4));
+
+
+
+		OrdersArray.clear();
+		System.out.println("============================");
+		System.out.println("Orders Done - " +DoneOrdersArray);
+		System.out.println("Orders Left - " +OrdersArray);
+
+
+
+		System.out.println("============================");
+		System.out.println("No. of orders left -" +OrdersArray.size());
+		System.out.println("No. of orders done -" +DoneOrdersArray.size());
+		
+	}
+	
+}
